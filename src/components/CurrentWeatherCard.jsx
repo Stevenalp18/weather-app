98b34b59ade479f6aea262afd53d05c3f31b6347
Steven Perez <stevenalp18@gmail.com>
2023-08-29 23:00:03 -0400

    working with api still, had issues, resolving

--- src/components/CurrentWeatherCard.jsx
+++ src/components/CurrentWeatherCard.jsx
@@ -1,18 +1,49 @@
-import { useEffect, useState } from "react";
-import { currentDayData } from "../data/weather-data/currentDayData";
+import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 
 const CurrentWeatherCard = () => {
   const location = useSelector((state) => state.location.value);
-  //  USE a useEffect() hook here and keep api call separately here,
-  // was having error because we attempted to use double useeffects and were redoing what we did.
-  //  We have to make the call here and do the rest of the work here
 
-  // fetchData();
-  return (
-    <>
-      {}
-      <div className="bg-sky-100 py-6 mb-10 rounded-xl flex flex-col lg:flex-row flex-wrap lg:flex-nowrap lg:justify-between text-center px-4">
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fecthApi = async (location) => {
+      try {
+        const response = await fetch(
+          `https://jsonplaceholder.typicode.com/todos/${location}`
+        );
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        setLoading(false);
+        setData(data);
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
+    };
+
+    fecthApi(location);
+  }, [location]);
+  console.log(data);
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  return <div className="bg-slate-200 p-4 rounded-xl text-lg">{}</div>;
+};
+export default CurrentWeatherCard;
+
+{
+  // const location = useSelector((state) => state.location.value);
+  /* <div className="bg-sky-100 py-6 mb-10 rounded-xl flex flex-col lg:flex-row flex-wrap lg:flex-nowrap lg:justify-between text-center px-4">
         <div className="my-auto flex flex-col lg:flex-row lg:gap-8">
           <div className="my-auto text-6xl">48º</div>
           <div className="mx-auto my-2">SUN</div>
@@ -22,8 +53,5 @@ const CurrentWeatherCard = () => {
           <div>feels like 64º</div>
           <div>wind: 12mph</div>
         </div>
-      </div>
-    </>
-  );
-};
-export default CurrentWeatherCard;
+    </div> */
+}
--- src/data/weather-data/currentDayData.jsx
+++ src/data/weather-data/currentDayData.jsx
@@ -1,10 +1,20 @@
 import { apiKey } from "../key";
 import { currentApiExt, baseUrl, queryApiExt } from "../data";
+import { useSelector } from "react-redux";
+import { useState } from "react";
+// const location = useSelector((state) => state.location.value);
 
-export function currentDayData() {
-  // const location = useSelector((state) => state.location.value);
-  fetch("https://jsonplaceholder.typicode.com/todos/2")
-    .then((res) => res.json())
-    // .then((data) => console.log(data))
-    .catch((err) => console.log(err));
+export async function getCurrentWeather(location) {
+  try {
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/users/${location}`
+    );
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    throw new Error(err);
+  }
 }
