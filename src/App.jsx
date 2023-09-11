18dcddbd5d78ae2601ba30e5d0a8b09127676d2c
Steven Perez <stevenalp18@gmail.com>
2023-09-10 21:43:20 -0400

    added my specialty side bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
 import DisplayBody from "./components/DisplayBody";
 import AlertModalWIP from "./components/AlertModalWIP";
+import SocialSideBar from "./components/SocialSideBar";
 
 function App() {
   return (
     <>
+      <SocialSideBar />
       <AlertModalWIP />
       <Header />
       <DisplayBody />
